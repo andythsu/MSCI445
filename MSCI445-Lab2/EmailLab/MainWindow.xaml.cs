d0a335e3409de177cb817cbb8aaf2b6634947560
Andy <f2280c@gmail.com>
2020-06-02 11:52:10 -0400

    added textbox

--- MSCI445-Lab2/EmailLab/MainWindow.xaml.cs
+++ MSCI445-Lab2/EmailLab/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,25 @@ namespace EmailLab
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		readonly TextBox TO_TEXTBOX;
+		readonly TextBox FROM_TEXTBOX;
+		readonly TextBox PASSWORD_TEXTBOX;
+		readonly TextBox SUBJECT_TEXTBOX;
+		readonly TextBox MESSAGE_TEXTBOX;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			TO_TEXTBOX = (TextBox) this.FindName("to_textbox");
+			FROM_TEXTBOX = (TextBox)this.FindName("from_textbox");
+			PASSWORD_TEXTBOX = (TextBox)this.FindName("password_textbox");
+			SUBJECT_TEXTBOX = (TextBox)this.FindName("subject_textbox");
+			MESSAGE_TEXTBOX = (TextBox)this.FindName("message_textbox");
 		}
-	}
+
+		private void Submit_Button_Click(object sender, RoutedEventArgs e)
+        {
+			
+        }
+    }
 }
