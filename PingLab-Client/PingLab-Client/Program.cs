63ef8c6ca2eacb235c39d46d9f1a22933c8efeb7
Andy <f2280c@gmail.com>
2020-07-03 21:58:13 -0400

    added server and client for ping lab

---
+++ PingLab-Client/PingLab-Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace PingLab_Client
+{
+    class Program
+    {
+        private const string HOST = "127.0.0.1";
+        private const int PORT = 1055;
+
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(HOST, PORT);
+            int n = 10;
+            for (int i = 0; i < n; i++)
+            {
+                string payload = "PING" + " " + i + " " + DateTime.Now.ToString() + "\r\n";
+                Console.WriteLine(payload);
+            }
+        }
+    }
+}
